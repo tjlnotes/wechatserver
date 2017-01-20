50f601f5140c502cc623743592c68c28d30d11e2
tanjinlin <tanjinlin@aircos.com>
2017-01-20 14:28:55 +0800

    import util

--- wechat/g.js
+++ wechat/g.js
@@ -3,6 +3,7 @@
 var sha1 = require('sha1');
 var getRawBody = require('raw-body');
 var Wechat = require('./wechat');
+var util = require('./util');
 
 module.exports = function(opts) {
     var wechat = new Wechat(opts);
