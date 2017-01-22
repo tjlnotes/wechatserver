a6161439b6da91d670a296ef0e3f4ced70f02264
tanjinlin <tanjinlin@aircos.com>
2017-01-22 19:13:46 +0800

    fix tpl

--- weixin.js
+++ weixin.js
@@ -3,6 +3,9 @@
 exports.reply = function* (next) {
     var message = this.weixin;
     if (message.MsgType === 'event') {
+        console.log("--------------message:" + message);
+        console.log(message.MsgType);
+        console.log(message.Event);
         if (message.Event === 'subscribe') {
             if (message.EventKey) {
                 console.log('扫描二维码进来：' + message.EventKey + '' + message.ticket)
