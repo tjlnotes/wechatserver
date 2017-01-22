f004937202e24da1a55260583372c4e520b3e1b3
tanjinlin <tanjinlin@aircos.com>
2017-01-22 16:11:56 +0800

    reply

--- app.js
+++ app.js
@@ -4,26 +4,13 @@ var Koa = require('koa');
 var path = require('path');
 var wechat = require('./wechat/g');
 var util = require('./libs/util');
+var config = require('./config');
+var weixin = require('./weixin');
 var wechat_file = path.join(__dirname, './config/wechat.txt');
-
-var config = {
-    wechat: {
-        appID: "wx9ae0bdfd2f1e5816",
-        appSecret: "0623f7ed6f3082060a69f5e4a77c2652",
-        token: "iloveyangmi",
-        getAccessToken: function() {
-            return util.readFileAsync(wechat_file);
-        },
-        saveAccessToken: function(data) {
-            data = JSON.stringify(data);
-            return util.writeFileAsync(wechat_file, data);
-        }
-    }
-}
 
 var app = new Koa();
 
-app.use(wechat(config.wechat));
+app.use(wechat(config.wechat, weixin.reply));
 
 app.listen(1234);
 console.log("listen 1234")
---
+++ weixin.js
@@ -0,0 +1,21 @@
+'use strict'
+
+exports.reply = function* (next) {
+    var message = this.weixin;
+
+    if (message.MsgType === 'event') {
+        if (message.Event === 'subscribe') {
+            if (message.EventKey) {
+                console.log('扫描二维码进来：' + message.EventKey + '' + message.ticket)
+            }
+
+            this.body = '你订阅了这个号\r\n' + '消息ID:' + message.MsgId;
+        }
+        else if (message.Event === 'unsubscribe') {
+            console.log('取消关注');
+        }
+    }
+    else {
+
+    }
+}
